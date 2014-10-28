714544aab7f61d1647eb68410fb28a32e1e87323
mitzip <mitzip@funtoo.org>
2014-10-28 01:58:26 -0500

    Initial Commit

---
+++ bin/fun2.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var cli  = require('commander');
+var fun2 = require('fun2');
+
+cli
+.version('0.0.1')
+.parse(process.argv);
---
+++ index.js
@@ -0,0 +1,18 @@
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('util').inherits;
+var helpwith = require('./lib/helpwith.js');
+var ports = require('./lib/ports.js');
+
+// **Fun2** is an ...
+function Fun2() {
+  EventEmitter.call(this);
+  this._stream = stream;
+
+  var scope = this;
+};
+
+inherits(Fun2, EventEmitter);
+
+module.exports = Fun2;
+
+
